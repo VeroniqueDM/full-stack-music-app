2365efbf29034cdb56b29609301d0e7108f0e7a2
VeroniqueDM <veronika.dmiteva@gmail.com>
2022-11-08 22:37:22 +0100

    added redirect on home page

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
 
-from .views import GetRoom, JoinRoom, RoomView, CreateRoomView
+from .views import GetRoom, JoinRoom, RoomView, CreateRoomView, UserInRoom
 
 urlpatterns = [
   
@@ -9,4 +9,5 @@ urlpatterns = [
     path('create-room', CreateRoomView.as_view()),
     path('get-room', GetRoom.as_view()),
     path('join-room', JoinRoom.as_view()),
+    path('user-in-room', UserInRoom.as_view()),
 ]
--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -6,19 +6,58 @@ import {
     Route,
     Link,
     Routes,
-    useNavigate,
+    Navigate,
 } from "react-router-dom";
 import Room from "./Room";
+import { Button, ButtonGroup, Grid, Typography } from "@material-ui/core";
 
 export default class HomePage extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+          roomCode: null,
+        };
+    }
+    async componentDidMount() {
+      fetch("/api/user-in-room")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data.code)
+          this.setState({
+            roomCode: data.code,
+          });
+          console.log(this.state)
+        });
+    }
+  
+    renderHomePage() {
+      return (
+        <Grid container spacing={3}>
+          <Grid item xs={12} align="center">
+            <Typography variant="h3" compact="h3">
+              House Party
+            </Typography>
+          </Grid>
+          <Grid item xs={12} align="center">
+            <ButtonGroup disableElevation variant="contained" color="primary">
+              <Button color="primary" to="/join" component={Link}>
+                Join a Room
+              </Button>
+              <Button color="secondary" to="/create" component={Link}>
+                Create a Room
+              </Button>
+            </ButtonGroup>
+          </Grid>
+        </Grid>
+      );
     }
     render() {
         return (
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<p>This is the home page</p>} />
+                    <Route path="/" element={
+                      this.state.roomCode ? <Navigate to={`/room/${this.state.roomCode}`} /> : (this.renderHomePage())
+                    } />
                     <Route path="/join" element={<RoomJoinPage />} />
                     <Route path="/create" element={<CreateRoomPage />} />
                     <Route path="/room/:roomCode" element={<Room />} />
